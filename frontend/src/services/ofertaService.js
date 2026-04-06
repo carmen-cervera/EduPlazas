c005667b9afda5de0adeb596e22026f9ab9627f1
maurobartolome <mauro.bartolome@alumnos.upm.es>
2026-04-06 13:20:45 +0200

    Update ofertaService.js

--- frontend/src/services/ofertaService.js
+++ frontend/src/services/ofertaService.js
@@ -5,3 +5,5 @@ const BASE_URL = 'http://localhost:8080/ofertas'
 export const publicarOferta = (usuarioId, grado, plazas) =>
   axios.post(BASE_URL, { usuarioId, grado, plazas })
 
+export const obtenerOfertasMiUniversidad = (usuarioId) =>
+  axios.get(`${BASE_URL}/mi-universidad`, { params: {usuarioId} })
